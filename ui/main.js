42c0ee4257dd382d9f979c77582a262c85b6de17
madhavaraju-am <30906451+madhavaraju-am@users.noreply.github.com>
2017-08-26 17:24:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,11 @@ element.innerHTML = "New VALUE !!!!!" ;
 
 //move the image
 var img = document.getElementById('monster');
+var marginleft =0;
+function moveRight() {
+  marginleft = marginleft +10;
+  img.style.marginLeft = marginleft +'px';
+}
 img.onclick = function() {
     img.style.marginLeft = '100px';
 };
