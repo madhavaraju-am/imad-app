60e4619a893d54c52939b0d9c7d1d705c500a9b9
madhavaraju-am <30906451+madhavaraju-am@users.noreply.github.com>
2017-08-27 12:52:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg'), Pool;
+
+var config = {
+    user: 'madhavarajuam',
+    database: 'madhavarajuam',
+    host: 'http://db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
+
 
 var app = express();
 app.use(morgan('combined'));
@@ -8,6 +18,8 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+
 
 var articles = {
     'article-one': {
@@ -96,7 +108,18 @@ app.get('/:articleName', function (req,res) {
    res.send(createTemplate(articles[articleName]));
 });
 
-
+var pool = new Pool(config);
+app.get('/testt', function (req, res) {
+    //get the data from db and return the results
+  pool.query('SELECT * FROM testt', function(err,result) {
+      if(err){
+            res.status(500), send(err.toString());
+      }
+      else {
+        res.send(JSON.stringify(result));
+      }
+  });
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
